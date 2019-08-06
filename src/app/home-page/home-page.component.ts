a33cad818a5134792b448fe973e9a0223ccbfce0
James Irick <james.e.irick@hotmail.com>
2019-08-06 19:43:06 -0400

    wip

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  OnInit,
+  OnDestroy } from '@angular/core';
+import { HostListener } from "@angular/core";
+import { debounceTime } from "rxjs/operators";
+import { Subject } from 'rxjs/internal/Subject';
 
 @Component({
     selector: 'app-home',
@@ -6,6 +15,44 @@ import { Component } from '@angular/core';
     styleUrls: ['./home-page.component.scss']
   })
 
-export class HomePageComponent {
-    
+export class HomePageComponent implements OnInit, OnDestroy, AfterViewInit {
+  
+  public isSticky: boolean = false;
+  public isAnimated: boolean = false; 
+
+  @ViewChild('header', {static: false}) header: ElementRef;
+  private navbarOffsetTop: number;
+  private resizeObservable;
+
+  private pageYOffsetSubject = new Subject<number>();
+
+  constructor() {}
+
+  @HostListener("window:scroll", ['$event'])
+  onWindowScroll(event: Event|any) {
+    this.pageYOffsetSubject.next(event.path[1].pageYOffset)
+  }
+
+  ngOnInit(): void {
+    this.resizeObservable = this.pageYOffsetSubject
+                                .asObservable()
+                                .pipe(debounceTime(15))
+                                .subscribe((e:number) => this.onWindowScrollEvent(e))
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeObservable != null) {
+      this.resizeObservable.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.navbarOffsetTop = this.header.nativeElement.offsetTop;
+  }
+
+  private onWindowScrollEvent(pageYOffset: number): void {
+    this.isAnimated = true;
+    this.isSticky = pageYOffset > (this.navbarOffsetTop - 150)
+  }
+
 }
